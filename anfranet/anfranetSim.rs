4bf1f8b573dbf02719edb18bb3b48032994982ac
LORTEY <103606175+LORTEY@users.noreply.github.com>
2024-12-13 21:29:29 +0000

    Update anfranetSim.rs

--- anfranet/anfranetSim.rs
+++ anfranet/anfranetSim.rs
@@ -1,3 +1,3 @@
 fn main(){
- println!("another day spent learning rust");
+ println!("another day spent learning rust sorry just bloat commit but i am still learning and cant resume the project untill i have succeded");
 }
